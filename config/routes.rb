9912274b761f1ecaa971ae5b914068d720d9f5ce
Nidhi <nidhishah1311@gmail.com>
2018-03-26 10:43:02 +0530

    Deploy on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
   end
   
-
+  root 'todos#index'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   post 'auth/logout', to: 'authentication#logout'

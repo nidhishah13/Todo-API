c062e504d0a29fefc102ce9729ded8b9ba0e5ea2
Nidhi <nidhishah1311@gmail.com>
2018-06-01 16:50:45 +0530

    rack-cors configuration

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module TodosApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+                 headers: :any,
+                 expose: %w[X-Pagination Authorization],
+                 methods: %i[get post options patch put delete head]
       end
     end
 
